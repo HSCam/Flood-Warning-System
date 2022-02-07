4341fba2ce3084f7647ad9cb5827a6c8296d5f6a
sinhah-harsh <68511134+sinhah-harsh@users.noreply.github.com>
2022-02-07 22:28:11 +0000

    Task 1D draft 1

---
+++ Task1D.py
@@ -0,0 +1,26 @@
+from floodsystem.geo import river_with_stations
+from floodsystem.geo import stations_by_river
+from floodsystem.stationdata import build_station_list
+
+stations = build_station_list()  # printing a list of rivers with at least one monitoring station (all contained in 'rivers' variable), followed by output of first ten alphabetically
+rivers = river_with_stations(stations)
+print("Stations:", len(rivers), "\n", "First 10 (alphabetically):", rivers[:10], "\n")
+
+stations_on_river = stations_by_river(stations)
+testA = stations_on_river["River Aire"]
+namesA = []
+for i in range(len(testA)):
+    namesA.append(testA[i].name)
+print("Stations on the River Aire:",sorted(namesA),"\n")
+
+testB = stations_on_river["River Cam"]
+namesB = []
+for i in range(len(testB)):
+    namesB.append(testB[i].name)
+print("Stations on the River Cam:",sorted(namesB),"\n")
+
+testC = stations_on_river["River Thames"]
+namesC = []
+for i in range(len(testC)):
+    namesC.append(testC[i].name)
+print("Stations on the River Thames:", sorted(namesC), "\n")
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,23 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+
+#Exercise 1D
+def river_with_stations(stations):
+    station_rivers = set([])  #using a 'set' object for this function as it only allows unique entries. This means the data doesn't need to be manually filtered to prevent duplicates.
+    for stat in stations:
+        station_rivers.add(stat.river)
+    return sorted(station_rivers)  # returns a sorted list of the stations
+
+
+def stations_by_river(stations):  # Maps river names to stations
+    stations_on_river = {}
+    rivers = river_with_stations(stations)
+    for i in rivers:  # for loop through the list of rivers with stations
+        stats_on_river = []  # creates a blank dictionary to link the rivers with their stations
+        for x in stations:
+            if x.river == i:
+                stats_on_river.append(x)
+        stations_on_river[i] = list(stats_on_river) # adds lists to the dictionary
+    return stations_on_river
+

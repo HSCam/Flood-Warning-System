00380fcba187d616870910f4d886f5aa6e4de584
Grace Phan <thtp2@cam.ac.uk>
2022-02-08 21:02:27 +0000

    Draft 2

--- Task1C.py
+++ Task1C.py
@@ -1,10 +1,14 @@
-from floodsystem.geo import stations_within_radius
-from floodsystem.stationdata import build_station_list
+  def run():
+    from floodsystem.geo import stations_within_radius
+    from floodsystem.stationdata import build_station_list
 
 
-stations = build_station_list()
-within_10 =  stations_within_radius(stations, (52.2053, 0.1218), 10)
-stations_within_10 = []
-for station in within_10:
-    stations_within_10.append(station)
-print(sorted(stations_within_10))
+    stations = build_station_list()
+    within_10 =  stations_within_radius(stations, (52.2053, 0.1218), 10)
+    stations_within_10 = []
+    for station in within_10:
+        stations_within_10.append(station)
+    print(sorted(stations_within_10))
+
+if __name__ == "__main__":
+    run()

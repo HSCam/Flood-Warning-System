f1407baf3689d0b8bd2989c0fae24e90f074b93c
sinhah-harsh <68511134+sinhah-harsh@users.noreply.github.com>
2022-02-08 16:17:39 +0000

    Task 1D Draft 2 (corrected format to include __name__ = "__main__" block

--- Task1D.py
+++ Task1D.py
@@ -2,25 +2,29 @@ from floodsystem.geo import river_with_stations
 from floodsystem.geo import stations_by_river
 from floodsystem.stationdata import build_station_list
 
-stations = build_station_list()  # printing a list of rivers with at least one monitoring station (all contained in 'rivers' variable), followed by output of first ten alphabetically
-rivers = river_with_stations(stations)
-print("Stations:", len(rivers), "\n", "First 10 (alphabetically):", rivers[:10], "\n")
+def run():
+    stations = build_station_list()  # printing a list of rivers with at least one monitoring station (all contained in 'rivers' variable), followed by output of first ten alphabetically
+    rivers = river_with_stations(stations)
+    print("Stations:", len(rivers), "\n", "First 10 (alphabetically):", rivers[:10], "\n")
 
-stations_on_river = stations_by_river(stations)
-testA = stations_on_river["River Aire"]
-namesA = []
-for i in range(len(testA)):
-    namesA.append(testA[i].name)
-print("Stations on the River Aire:",sorted(namesA),"\n")
+    stations_on_river = stations_by_river(stations)
+    testA = stations_on_river["River Aire"]
+    namesA = []
+    for i in range(len(testA)):
+        namesA.append(testA[i].name)
+    print("Stations on the River Aire:",sorted(namesA),"\n")
 
-testB = stations_on_river["River Cam"]
-namesB = []
-for i in range(len(testB)):
-    namesB.append(testB[i].name)
-print("Stations on the River Cam:",sorted(namesB),"\n")
+    testB = stations_on_river["River Cam"]
+    namesB = []
+    for i in range(len(testB)):
+        namesB.append(testB[i].name)
+    print("Stations on the River Cam:",sorted(namesB),"\n")
 
-testC = stations_on_river["River Thames"]
-namesC = []
-for i in range(len(testC)):
-    namesC.append(testC[i].name)
-print("Stations on the River Thames:", sorted(namesC), "\n")
+    testC = stations_on_river["River Thames"]
+    namesC = []
+    for i in range(len(testC)):
+        namesC.append(testC[i].name)
+    print("Stations on the River Thames:", sorted(namesC), "\n")
+
+if __name__ = "__main__":
+    run()

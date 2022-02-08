253a4e1618255fd014d9b0c1a7cb645de51fc5d0
Grace Phan <thtp2@cam.ac.uk>
2022-02-08 21:03:08 +0000

    Draft 2

--- Task1B.py
+++ Task1B.py
@@ -1,14 +1,17 @@
-from floodsystem.stationdata import build_station_list
-from floodsystem.geo import stations_by_distance
+def run():
+    from floodsystem.stationdata import build_station_list
+    from floodsystem.geo import stations_by_distance
 
-stations = build_station_list()
-station_distances =  stations_by_distance(stations, (52.2053, 0.1218))
-stations2 = []
-for station in station_distances:
-    stations2.append((station[0].name, station[0].town,station[1]))
+    stations = build_station_list()
+    station_distances =  stations_by_distance(stations, (52.2053, 0.1218))
+    stations2 = []
+    for station in station_distances:
+        stations2.append((station[0].name, station[0].town,station[1]))
 
-closest = stations2[:10]
-furthest = stations2[-10:]
+    closest = stations2[:10]
+    furthest = stations2[-10:]
 
-print("The 10 closest stations are:", closest)
-print("The 10 furthest stations are:", furthest)
+    print("The 10 closest stations are:", closest)
+    print("The 10 furthest stations are:", furthest)
+if __name__ == "__main__":
+    run()

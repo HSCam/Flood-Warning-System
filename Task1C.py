14927fdae596d246b34a263d5af33d209ea9e28d
Grace Phan <thtp2@cam.ac.uk>
2022-02-08 20:27:22 +0000

    draft 1C

--- Task1C.py
+++ Task1C.py
@@ -1,6 +1,7 @@
 from floodsystem.geo import stations_within_radius
 from floodsystem.stationdata import build_station_list
 
+
 stations = build_station_list()
 within_10 =  stations_within_radius(stations, (52.2053, 0.1218), 10)
 stations_within_10 = []

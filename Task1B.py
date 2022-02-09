6df8faf91ba164145267a5fad7ced00017b7b661
Grace Phan <thtp2@cam.ac.uk>
2022-02-09 06:48:07 +0000

    Final task 1B

--- Task1B.py
+++ Task1B.py
@@ -5,11 +5,14 @@ def run():
     """Requirements for Task 1B"""
 
     stations = build_station_list()
+    #calculates distance between each station and cambridge city centre
     station_distances =  stations_by_distance(stations, (52.2053, 0.1218))
     stations2 = []
+    #iterates through the distances and appends the relevant items to stations2
     for station in station_distances:
         stations2.append((station[0].name, station[0].town,station[1]))
-
+    
+    #list of closest and furthest objects in the list
     closest = stations2[:10]
     furthest = stations2[-10:]
 

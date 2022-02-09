de51fd1efe87c9ef74e6d1e637de69149ccc0bc5
Grace Phan <thtp2@cam.ac.uk>
2022-02-09 06:51:50 +0000

    Test 1C final

--- test_TaskC.py
+++ test_TaskC.py
@@ -1,7 +1,5 @@
-from sklearn.neighbors import radius_neighbors_graph
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import stations_within_radius
-import numpy as np
 
 stations = build_station_list()
 def test_stations_within_radius():

d56f007b8ad9482a090674908f5e501a1740e202
Grace Phan <thtp2@cam.ac.uk>
2022-02-09 04:50:09 +0000

    Adding Docstrings

--- Task1B.py
+++ Task1B.py
@@ -1,6 +1,8 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_by_distance
+
 def run():
-    from floodsystem.stationdata import build_station_list
-    from floodsystem.geo import stations_by_distance
+    """Requirements for Task 1B"""
 
     stations = build_station_list()
     station_distances =  stations_by_distance(stations, (52.2053, 0.1218))
@@ -13,5 +15,7 @@ def run():
 
     print("The 10 closest stations are:", closest)
     print("The 10 furthest stations are:", furthest)
+
 if __name__ == "__main__":
+    print("***Task 1B: CUED Part IA FLood Warning system")
     run()

79fe07731d7c95617cd39660d76fbeefec989a0a
Grace Phan <thtp2@cam.ac.uk>
2022-02-09 06:50:09 +0000

    Task 1C final

--- Task1C.py
+++ Task1C.py
@@ -5,8 +5,10 @@ def run():
     """Requirements for Task 1C"""
 
     stations = build_station_list()
+    #uses function to calculate stations within 10 of the centre given
     within_10 =  stations_within_radius(stations, (52.2053, 0.1218), 10)
     stations_within_10 = []
+    #iterates through stations and appends all items to station_within_10
     for station in within_10:
         stations_within_10.append(station)
     print(sorted(stations_within_10))

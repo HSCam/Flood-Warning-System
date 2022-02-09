c95be024f165c512b9cbfef2678ad8b233f6b180
Grace Phan <thtp2@cam.ac.uk>
2022-02-09 16:51:11 +0000

    final

--- test_TaskC.py
+++ test_TaskC.py
@@ -4,6 +4,6 @@ from floodsystem.geo import stations_within_radius
 
 stations = build_station_list()
 def test_stations_within_radius():
-    closest_station = stations_within_radius(stations, (52.2053, 0.1218), 1)
-    assert closest_station == ["Cambridge Jesus Lock"]
+    closest_station = stations_within_radius(stations, (52.2053, 0.1218), 10)
+    assert closest_station[0].name == "Cambridge Jesus Lock"
  

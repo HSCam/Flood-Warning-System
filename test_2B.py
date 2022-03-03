5ee620cd9d35c181264f513ec253868f1e1a38f1
sinhah-harsh <68511134+sinhah-harsh@users.noreply.github.com>
2022-03-03 00:34:21 +0000

    Fixed 2B test

--- test_2B.py
+++ test_2B.py
@@ -3,18 +3,10 @@
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_level_over_threshold
 
-def test1_stations_over_threshold():
-    """testing for sensible relative water levels"""
-    stations = build_station_list()
-    update_water_levels(stations)
-    stations = stations[:100]
-    for station in stations:
-        # print(type((station.relative_water_level())))
-        assert station.relative_water_level()<100 and station.relative_water_level()>-100
 
-def test2_stations_over_threshold():
+def test1_stations_over_threshold():
     """testing appropriate stations are excluded"""
     stations = build_station_list()
     stations = stations[:20]
-    stations2 = stations_level_over_threshold(stations, 2000)
-    assert len(stations2) == 0
+    stations1 = stations_level_over_threshold(stations, 2000)
+    assert len(stations1) == 0

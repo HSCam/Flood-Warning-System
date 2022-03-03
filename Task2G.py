92b1382f0ffd990f49fb5e9e1aaf091913671054
sinhah-harsh <68511134+sinhah-harsh@users.noreply.github.com>
2022-03-03 06:09:32 +0000

    2G initiation

--- Task2G.py
+++ Task2G.py
@@ -2,3 +2,12 @@ from floodsystem.stationdata import build_station_list
 from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.station import inconsistent_typical_range_stations
 from floodysystem.analysis import polyfit
+from floodsystem.plot import plot_water_level_with_fit
+import datetime
+import numpy as np
+
+"""Task 2G: issuing flood warnings for towns"""
+'''Logic used for this code:
+ - checks the data for recent days and tries to assign it an equation (to have a gradient function)
+ - checks the 
+
